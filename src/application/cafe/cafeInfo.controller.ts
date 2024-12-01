f9d137d60af73e68c1a92066aa51607dd4c52d5f
chainbell <mylifeis123@naver.com>
2024-12-01 09:17:26 +0000

    ✏️ fix: cafeInfo api description 수정

--- src/application/cafe/cafeInfo.controller.ts
+++ src/application/cafe/cafeInfo.controller.ts
@@ -10,14 +10,14 @@ import { CafeJoinUserCombinedCountResDto } from "./dto/res/CafeJoinUserCombinedR
 
 
 
-@ApiTags('cafeInfo API')
+@ApiTags('카페 정보 API')
 @Controller('/api/v1/cafeInfo')
 export class CafeInfoController {
 
   constructor(private readonly cafeInfoService: CafeInfoService) { }
 
   @Get('/user-count')
-  @ApiResponse({ status: 200, description: 'Success', type: CafeJoinUserCombinedCountResDto })
+  @ApiResponse({ status: 200, description: '가입자 수(기준, 최신 동시) 조회', type: CafeJoinUserCombinedCountResDto })
   async getCombine(): Promise<CafeJoinUserCombinedCountResDto> {
 
     const combinedCount: Map<string, number> = await this.cafeInfoService.getCombinedCafeJoinedUserCount();
@@ -27,7 +27,7 @@ export class CafeInfoController {
 
   @Get('/user-count/:type')
   @ApiParam({ name: 'type', enum: WakzooJoinedUserCountType, required: true, description: 'Type of user count' })
-  @ApiResponse({ status: 200, description: 'Success', type: CafeJoinUserCountResDto })
+  @ApiResponse({ status: 200, description: '가입자 수(기준, 최신 중 하나) 조회', type: CafeJoinUserCountResDto })
   async get(@Param('type') type: WakzooJoinedUserCountType): Promise<CafeJoinUserCountResDto> {
 
     const joinUserCount: number = await this.cafeInfoService.getCafeJoinedUserCount(type.toString());
@@ -36,7 +36,7 @@ export class CafeInfoController {
   }
 
   @Post('/user-count')
-  @ApiResponse({ status: 200, description: 'Success', type: CafeJoinUserCountResDto })
+  @ApiResponse({ status: 200, description: '가입자 수 저장(카페->DB)', type: CafeJoinUserCountResDto })
   async post(): Promise<CafeJoinUserCountResDto> {
 
     this.cafeInfoService.setCafeJoinedUserCount(WakzooJoinedUserCountType.RECENT);
